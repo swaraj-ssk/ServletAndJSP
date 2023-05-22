591fa07d24b2de16111e77fb3cfc4ebe57ef39bf
swaraj-ssk <swarajkalbande123@gmail.com>
2023-05-23 02:21:58 +0530

    Two servlets communicating

--- src/main/java/com/VIT/SqServlet.java
+++ src/main/java/com/VIT/SqServlet.java
@@ -1,11 +1,19 @@
 package com.VIT;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SqServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
+		int k = (int) req.getAttribute("k");
+		k= k*k;
+		PrintWriter out = res.getWriter();	
+		out.println("Square of sum is " + k);
 	}
 }
