1e6a17b3951ace8359f69cd09ee1db5a45d9000c
swaraj-ssk <swarajkalbande123@gmail.com>
2023-06-04 11:56:16 +0530

    Done with HTTPSession management

--- src/main/java/com/VIT/AddServlet.java
+++ src/main/java/com/VIT/AddServlet.java
@@ -1,20 +1,18 @@
 package com.VIT;
 
+import java.io.IOException;
+
 import javax.servlet.http.*;
 
 public class AddServlet extends HttpServlet {
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-		try {
-			res.getWriter().println("result is " + k);
-		} catch (Exception e) {
-			e.printStackTrace();
-		};
+		res.sendRedirect("sq?k="+k);
 	}
 }
--- src/main/java/com/VIT/SqServlet.java
+++ src/main/java/com/VIT/SqServlet.java
@@ -1,11 +1,17 @@
 package com.VIT;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SqServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse res) {
-		
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int k = Integer.parseInt(req.getParameter("k"));
+		k = k*k;
+		PrintWriter out = res.getWriter();
+		out.println("Square is "+k);
 	}
 }
